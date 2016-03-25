d4cf08eb1f226a9b9206009c2a6b590b9c913d41
Feixuan Chi <feixuan.chi@mail.mcgill.ca>
2016-03-25 17:49:05 -0400

    add the initBuffer method, BOOLEAN method, rewind

--- parse.c
+++ parse.c
@@ -1,23 +1,81 @@
 #include<stdio.h>
-
-void intBuffer(char *inputLine);
-//initialize the private parse.c buffer with the string passed in the parameter inputLine
-
-BOOLEAN hasNextToken();
-//This function returns a 1 if there are still more tokens in the string, otherwise it returns 0. For example, if this is a string "my name is Bob", then this string has 4 tokens.
+#include<string.h>
+#include<stlib.h>
 #define BOOLEAN int
-#define TRUE 1
-#define FALSE 0
-BOOLEAN x;
-int y;
-if (z == TRUE) ......
-return FALSE;
+#define true 1
+#define false 0
+
+char *buffer;
+int bsize;
+char *safe;
 
 
-
+void initBuffer(char *inputLine);
+//initialize the private parse.c buffer with the string passed in the parameter inputLine
+BOOLEAN hasNextToken();
+//This function returns a 1 if there are still more tokens in the string, otherwise it returns 0. For example, if this is a string "my name is Bob", then this string has 4 tokens
 char *nextToken();
-//This function removes the first token from the buffer. The buffer is reduced in size since the token was removed. The removed token is returned to the caller. If the buffer is empty then this function returns NULL.
+void rewind();
+
+void initBuffer(char *inputLine){
+	int len = strlen(inputLine);
+	buffer = (char *)malloc(len + 1);
+	strcpy(buffer,inputLine);
+ 
+	safe = inputLine;
+	//safe = (char *)malloc(len + 1);
+	//strcpy(safe,inputLine);
+}
+
+BOOLEAN hasNextToken(){
+	bsize = strlen(buffer);
+	if((char)buffer[0]=='\0'){
+		return 0;
+	}
+	else{
+		return 1;
+	}
+}
 
 
-//void rewind();
+
+
+//seperate into tokens
+char *nextToken(){
+	int line_num = 1;
+	char *arrToken; 
+	FILE *fp;
+	char temp[512];
+		
+
+   	//how to input file content line by line?
+   	//what is const char s[2]??
+   	//char str[80] = ;
+   	//const char s[2] = "-";
+
+
+   	/* get the first token */
+   	token = strtok(str, s);
+
+   	/* walk through other tokens */
+   	while( token != NULL ){
+ 
+	
+      	//	printf( " %s\n", token );
+      	//	print is just for checking/debugging
+		token = strtok(NULL, s);
+
+	//return the array contain the tokens
+	return 
+   	}
+	//do we need to increase the line number?
+	//line_num++;
+   	return NULL;
+}
+
+
+void rewind()
+{
+	initBuffer(safe);
+}
 
